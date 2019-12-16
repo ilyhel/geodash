dba71633bbf6ceb305a122ba0627cee96ff5a85c
ilyhel <mzoughi.ilyes@gmail.com>
2019-12-16 05:20:36 +0100

    Updates

---
+++ precache-manifest.e39fabbe1794e64684b6b39562e3d46b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6fe849be952d5df5b0f6b77201d4854",
+    "url": "/geodash/index.html"
+  },
+  {
+    "revision": "e44233dc2c2a1d5cb610",
+    "url": "/geodash/static/css/main.66930e45.chunk.css"
+  },
+  {
+    "revision": "f80bb1a6c0205a12d518",
+    "url": "/geodash/static/js/2.66528e14.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/geodash/static/js/2.66528e14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e44233dc2c2a1d5cb610",
+    "url": "/geodash/static/js/main.d70647c2.chunk.js"
+  },
+  {
+    "revision": "cac464891c0249cca19d",
+    "url": "/geodash/static/js/runtime-main.b5a7ea33.js"
+  }
+]);
